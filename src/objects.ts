f30ed0f579e0109052e33739347915e9d078eea2
yuri <yuri.barros.luz.familia@gmail.com>
2024-11-20 16:37:45 -0300

    adds

--- src/objects.ts
+++ src/objects.ts
@@ -21,3 +21,52 @@ const user: User = {
   password: '12345',
   orders:[{ productId: '1', price: 200}],
 };
+
+const printLog = (message: string) => {};
+
+printLog(user.password!);
+
+// Unions
+type Author = {
+  books: string[];
+};
+
+const author : Author & User = {
+  age:2,
+  books: ["1"],
+  email: "author@gmail.com",
+  firstName: "Felipe",
+  orders: [],
+};
+
+// Interfaces
+
+interface UserInterface {
+  readonly firstName: string;
+  email: string;
+  login(): string;
+}
+
+const emailUser: UserInterface = {
+  email: "felipe@gmail.com",
+  firstName: "Felipe",
+  login() {
+    return "a";
+  },
+};
+
+interface AuthorInterface {
+  books: string[];
+}
+
+const newAuthor: UserInterface & AuthorInterface = {
+  email: "author@gmail.com",
+  firstName: "Felipe",
+  books: [],
+  login() {
+    return "a";
+  },
+};
+
+type Grade = number | string;
+const grade: Grade = 1;
